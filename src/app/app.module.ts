61138c4b0fa8a50fa0b5618c274bb1916a28b825
HanenBS <bensalemhanen16@gmail.com>
2023-11-30 15:09:50 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainProfilComponent } from './main-profil/main-profil.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import {AngularMaterialModule} from "./shared/angular-material/angular-material.module";
+import { FooterComponent } from './footer/footer.component';
+import {NgOptimizedImage} from "@angular/common";
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MainComponent,
+    HeaderComponent,
+    MainHeaderComponent,
+    MainProfilComponent,
+    MainContentComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatIconModule,
+        MatButtonModule,
+        MatMenuModule,
+        FlexLayoutModule,
+        AngularMaterialModule,
+        NgOptimizedImage
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/main-profil/main-profil.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-profil',
+  templateUrl: './main-profil.component.html',
+  styleUrls: ['./main-profil.component.css']
+})
+export class MainProfilComponent {
+
+}
